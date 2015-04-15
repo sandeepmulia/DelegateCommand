9f820aa7424bc6f275453e0ebbadc04838e96ade
sandeepmulia <sandeep.m.1981@gmail.com>
2015-04-15 22:09:10 +1000

    Commit #1

---
+++ DelegateCommand/DelegateCommand/Command/DelegateCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DelegateCommand
+{
+    public class RoutedCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        public RoutedCommand(Action<object> action)
+        {
+            _execute = action;
+        }
+
+        public RoutedCommand(Action<object> action, Predicate<object> predicate)
+        {
+            _execute = action;
+            _canExecute = predicate;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            if (_execute != null)
+                _execute(parameter);
+        }
+    }
+}
---
+++ DelegateCommand/DelegateCommand/ViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DelegateCommand
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<string> o = new ObservableCollection<string>();
+        public ViewModel()
+        {            
+            o.Add("Sandeep");
+            o.Add("Sharath");
+            o.Add("Manasa");
+            o.Add("Vijaya");
+            o.Add("Shankar");
+            o.Add("Ram");
+            o.Add("Savithri");
+
+            ButtonCommand = new RoutedCommand(Execute, CanExecute);
+        }
+
+        private bool CanExecute(object obj)
+        {
+            return !String.IsNullOrEmpty(TextData);
+        }
+
+        private void Execute(object obj)
+        {
+            MessageBox.Show(String.Format("Command executed {0}", obj.ToString()));
+        }
+
+        public ICommand ButtonCommand
+        {
+            get; set; 
+        }
+
+        public ICommand Check { get; set; }
+
+        public ObservableCollection<string> Item
+        {
+            get { return o; }
+            set { o = value; }
+        }
+
+        private string _textData = string.Empty;
+
+        public String TextData
+        {
+            get
+            {
+               return _textData;
+            }
+
+            set
+            {
+                _textData = value;
+                if (PropertyChanged != null)
+                    PropertyChanged(this,new PropertyChangedEventArgs("TextData"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
